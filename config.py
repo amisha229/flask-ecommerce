b799428afd1cb463297b7cbf416664ef13523f67
Amisha Singh <singhamisha4002@gmail.com>
2026-02-25 10:57:33 +0530

    Update database credentials for EC2 deployment

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess-string'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+pymysql://root:amisha2004@localhost/flask_ecommerce'
+        'mysql+pymysql://flaskuser:amisha2004@localhost/flask_ecommerce'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
